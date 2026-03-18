b74722f7796b9d0182806fa7fc96ce88e9dc9dfa
Ly Ngo <ly@oven.bz>
2026-03-18 15:39:41 +0700

    feat/register: define global endpoint, modify table RefreshToken and build onModuleDestroy for Prisma

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -3,7 +3,11 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect()
-  }
+    async onModuleInit() {
+        await this.$connect()
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
 }
