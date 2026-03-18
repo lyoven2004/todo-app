9c22c9fd13c8cfa23bfbaa8e65f04e0467c1a572
Ly Ngo <ly@oven.bz>
2026-03-18 17:30:36 +0700

    chore/config-interceptor#12: implement response interceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { ResponseInterceptor } from './common/interceptors/transform-response.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,5 +14,8 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, }),);
+  app.useGlobalInterceptors(new ResponseInterceptor());
 }
 bootstrap();
