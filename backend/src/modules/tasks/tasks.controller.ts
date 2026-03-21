9916625eeaf7c3c1feacdbc003703dd6da261be0
lyoven2004 <ly@oven.bz>
2026-03-21 17:39:48 +0700

    feat/todo-app#21: rebase with develop and solve conflict

--- backend/src/modules/tasks/tasks.controller.ts
+++ backend/src/modules/tasks/tasks.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';import type { JwtPayload } from '../auth/types/jwt-payload.type';
+import { UpdateTaskDto } from './dto/update-task.dto'; import type { JwtPayload } from '../auth/types/jwt-payload.type';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('tasks')
@@ -26,10 +26,10 @@ export class TasksController {
     return this.tasksService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string, @Req() req: any) {
-    return this.tasksService.findOne(id, req.user.id);
+  findOne(@Param('id') id: string,
+    @CurrentUser('sub') userId: string) {
+    return this.tasksService.findOne(id, userId);
   }
 
   @Patch(':id')
