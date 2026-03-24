92bcb2fbc2d8eb21d40eaba4d20b8cf35b8adcfe
Ly Ngo <ly@oven.bz>
2026-03-24 17:13:46 +0700

    feat/todo-app#32: delete cookie setup for backend

--- frontend/src/axios/auth-api.ts
+++ frontend/src/axios/auth-api.ts
@@ -1,14 +1,8 @@
-
 import type {
   TLoginRequestDto,
   TLoginResponseDto,
 } from "@/app/login/_config/login.schema";
 import { api } from "@/lib/axios";
-import { TApiResponse } from "@/types/api.response";
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-type TBackendLoginData = TLoginResponseDto;
 
 export async function loginUser(
   payload: TLoginRequestDto,
