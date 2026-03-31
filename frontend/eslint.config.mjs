26cfc5ade400721310c45c19f37336cf27dd825a
Ly Ngo <ly@oven.bz>
2026-03-31 16:46:14 +0700

    chore/todo-app#66 : modify import

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import "eslint-config-next/core-web-vitals.js"
+import "eslint-config-next/typescript.js"
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
